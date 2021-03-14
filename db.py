68bf76fea50d069eb0fa41b7b5439e7ded2368be
Grace Gao <ggaoww@gmail.com>
2021-03-14 04:51:07 -0400

    added new data

---
+++ db.py
@@ -0,0 +1,28 @@
+
+import os
+import sqlite3
+
+from flask import current_app, g
+
+
+def get_db():
+    db = getattr(g, "db", None)
+    if db is None:
+        query = open(current_app.config["SCHEMA"], "r").read()
+        conn = sqlite3.connect(current_app.config["DATABASE"])
+        cursor = conn.cursor()
+        cursor.executescript(query)
+        conn.commit()
+        cursor.close()
+        db = g._database = sqlite3.connect(current_app.config["DATABASE"])
+    return db
+
+
+def close_connection(exception):
+    db = getattr(g, "db", None)
+    if db is not None:
+        db.close()
+
+
+def init_app(app):
+    app.teardown_appcontext(close_connection)
--- web.py
+++ web.py
@@ -15,6 +15,8 @@ def token_auth(f):
     return decorated_function
 
 
+# machine learning endpoints
+
 @app.route('/predict', methods=['POST'])
 @token_auth
 def predict():
@@ -32,6 +34,63 @@ def train():
     from engines import content_engine
     data_url = request.data.get('data-url', None)
     content_engine.train(data_url)
+    return {"message": "Success!", "success": 1}
+
+
+def send_email(address, name):
+    
+    # import smtplib
+    # from email.mime.text import MIMEText
+
+    # content = "hello"
+    # msg = MIMEText(message)
+
+    # # headers
+    # TO = 'ggaoww@gmail.com'
+    # msg['To'] = TO
+    # FROM = 'ggaoww@gmail.com'
+    # msg['From'] = FROM
+    # msg['Subject'] = 'Matchbox'
+
+    # # SMTP
+    # send = smtplib.SMTP('smtp.zoho.com', 587)
+    # send.starttls()
+    # send.login('from_user@domain.com', 'password')
+    # send.sendmail(FROM, TO, msg.as_string())
+    # send.quit()
+    print(address + " " + name)
+
+
+def queue(user, item_id):
+    # send_email(user['email'], user['name'])
+
+
+# POST method to create a new return
+@app.route('/returns/new', methods=['POST'])
+@token_auth
+def new():
+    return_json = request.get_json(force=True)
+    # saves image
+    image = request.files['image']
+    if image:
+        filename = secure_filename(image.filename)
+        image.seek(0)
+        image.save(os.path.join(current_app.config["UPLOAD_DIR"], filename))
+   
+    # saves item details
+    order_id = return_json['order_id']
+    item_id = data.get_item(order_id)
+
+    from engines import content_engine
+    matches = content_engine.predict(str(item), 10)
+
+    # sort the list in order of relevancy
+    matches = matches.sort(key=lambda x:x[1], reverse= True)
+    for match in matches:
+        users = find_users(match)
+    best = users[0]
+    queue(best, item_id)
+
     return {"message": "Success!", "success": 1}
 
 
